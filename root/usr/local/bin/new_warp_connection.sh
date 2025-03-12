8a27e01a4dfad0291f968db520b90f18205e25aa
Nguyễn Lê Thái Nguyên <24DH602234@st.huflit.edu.vn>
2025-03-13 06:51:37 +0700

    Update new_warp_connection.sh

--- root/usr/local/bin/new_warp_connection.sh
+++ root/usr/local/bin/new_warp_connection.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Đường dẫn đến file log của proxy server
-LOG_FILE="/var/log/wireproxy.log"
-
 # Hàm để ngắt kết nối và kết nối lại Warp
 function reset_warp_connection {
     warp-cli disconnect
     warp-cli connect
 }
 
-# Theo dõi file log để xác định các kết nối mới
-tail -F $LOG_FILE | while read LINE; do
-    if echo $LINE | grep -q "New connection"; then
+# Hàm để kiểm tra các kết nối mới
+function check_new_connections {
+    netstat -an | grep ESTABLISHED
+}
+
+# Theo dõi các kết nối mới
+while true; do
+    if check_new_connections | grep -q "ESTABLISHED"; then
         reset_warp_connection
     fi
+    sleep 5  # Kiểm tra mỗi 5 giây
 done
