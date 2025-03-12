47a710c41172b19cb6035dc3217d3074c97bb65d
Nguyễn Lê Thái Nguyên <24DH602234@st.huflit.edu.vn>
2025-03-12 20:10:01 +0700

    Update new_warp_connection.sh

--- root/usr/local/bin/new_warp_connection.sh
+++ root/usr/local/bin/new_warp_connection.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Disconnect existing Warp connection
-warp-cli disconnect
+# Đường dẫn đến file log của proxy server
+LOG_FILE="/var/log/wireproxy.log"
 
-# Connect to Warp to get a new IP
-warp-cli connect
+# Hàm để ngắt kết nối và kết nối lại Warp
+function reset_warp_connection {
+    warp-cli disconnect
+    warp-cli connect
+}
 
-# Start the proxy server
-exec wireproxy
+# Theo dõi file log để xác định các kết nối mới
+tail -F $LOG_FILE | while read LINE; do
+    if echo $LINE | grep -q "New connection"; then
+        reset_warp_connection
+    fi
+done
